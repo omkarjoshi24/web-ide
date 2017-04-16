f2a40441e5b3ebcd14f9d7517167887a291e7e2d
Omkar Joshi <omkar.joshi24@gmail.com>
2017-04-16 01:47:25 -0400

    added hash to geerated file name in webpack dev config

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   ],
   output: {
     path: path.join(__dirname, "/build/"),
-    filename: "[name].js",
+    filename: "[name]-[hash].js",
     publicPath: '/'
   },
   module: {
